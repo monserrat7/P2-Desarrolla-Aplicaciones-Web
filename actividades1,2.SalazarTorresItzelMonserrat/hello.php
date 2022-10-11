819c9386f2b818be3c73648e62b463c562c5cfc6
monserrat7 <114451599+monserrat7@users.noreply.github.com>
2022-10-10 23:36:37 -0500

    Add files via upload

---
+++ actividades1,2.SalazarTorresItzelMonserrat/hello.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HELLO WORLD</title>
+	<style type="text/css">
+		center{
+            background-color: #FADBD8;
+            width: 900px;
+            background: #FADBD8;
+            padding: 70px;
+            margin: auto;
+            margin-top: 40px;
+            border-radius: 4px;
+        }
+        h1{
+        	text-align: center;
+        }
+        button{
+        	font-family: candara;
+
+        }
+	</style>
+</head>
+<body bgcolor="#D1F2EB">
+<h1><FONT FACE="Magical Story" SIZE=28 COLOR="#1A5276"></FONT></h1>
+<center>
+
+<FONT FACE="Magical Story" SIZE=6 COLOR="#1A5276">
+<?php
+  echo 'Hello World'.'<br>'; 
+  echo 'Bienvenido al Mundo de PHP';
+
+?>
+
+</FONT>
+
+</body>
+</html>
